d55e2870e5f5d57ecff94da17f46a49ad20063ea
andrii <andriinkn@gmail.com>
2023-10-24 22:09:07 +0300

    added WelcomeScreen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,20 +1,51 @@
-import { useEffect } from 'react';
-import { Text } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Animated, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import WelcomeBg from '@/assets/welcome-bg.jpg';
 import { IPageProps } from '@/navigation/navigationTypes';
 import { ScreenNames } from '@/types/ScreenNames';
 
 export const WelcomeScreen: React.FC<IPageProps<ScreenNames.WELCOME>> = ({
   navigation,
 }): JSX.Element => {
+  const bgOpacity = useRef(new Animated.Value(0)).current;
+  const textOpacity = useRef(new Animated.Value(0)).current;
+
   useEffect(() => {
+    const bgAnimation = Animated.timing(bgOpacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: false,
+    });
+
+    const textAnimation = Animated.timing(textOpacity, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: false,
+    });
+
+    Animated.sequence([bgAnimation, textAnimation]).start();
+
     setTimeout(() => navigation.navigate(ScreenNames.HOME), 2500);
-  }, [navigation]);
+  }, [bgOpacity, navigation, textOpacity]);
 
   return (
-    <SafeAreaView className='grow items-center justify-center bg-black p-4'>
-      <Text className='text-3xl text-white'>Welcome Screen</Text>
+    <SafeAreaView className='grow'>
+      <Animated.Image
+        className='h-full w-full grow'
+        source={WelcomeBg}
+        resizeMode='cover'
+        style={{ opacity: bgOpacity }}
+      />
+      <View className='absolute top-1/2 w-full -translate-y-1/2 p-4'>
+        <Animated.Text
+          className='text-center text-3xl font-bold uppercase tracking-widest text-rose-300'
+          style={{ opacity: textOpacity }}
+        >
+          Welcome to the world of Star Wars
+        </Animated.Text>
+      </View>
     </SafeAreaView>
   );
 };
