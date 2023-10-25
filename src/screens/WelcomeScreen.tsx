c3a63a60f5189e239ecfd8a316101c06bd34449b
andrii <andriinkn@gmail.com>
2023-10-25 11:53:27 +0300

    removed animation

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from 'react';
-import { Animated, View } from 'react-native';
+import { useEffect } from 'react';
+import { ImageBackground, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import WelcomeBg from '@/assets/welcome-bg.jpg';
@@ -9,43 +9,17 @@ import { ScreenNames } from '@/types/ScreenNames';
 export const WelcomeScreen: React.FC<IPageProps<ScreenNames.WELCOME>> = ({
   navigation,
 }): JSX.Element => {
-  const bgOpacity = useRef(new Animated.Value(0)).current;
-  const textOpacity = useRef(new Animated.Value(0)).current;
-
   useEffect(() => {
-    const bgAnimation = Animated.timing(bgOpacity, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: false,
-    });
-
-    const textAnimation = Animated.timing(textOpacity, {
-      toValue: 1,
-      duration: 2000,
-      useNativeDriver: false,
-    });
-
-    Animated.sequence([bgAnimation, textAnimation]).start();
-
     setTimeout(() => navigation.navigate(ScreenNames.HOME), 2500);
-  }, [bgOpacity, navigation, textOpacity]);
+  }, [navigation]);
 
   return (
     <SafeAreaView className='grow'>
-      <Animated.Image
-        className='h-full w-full grow'
-        source={WelcomeBg}
-        resizeMode='cover'
-        style={{ opacity: bgOpacity }}
-      />
-      <View className='absolute top-1/2 w-full -translate-y-1/2 p-4'>
-        <Animated.Text
-          className='text-center text-3xl font-bold uppercase tracking-widest text-rose-300'
-          style={{ opacity: textOpacity }}
-        >
+      <ImageBackground className='grow justify-center' source={WelcomeBg} resizeMode='cover'>
+        <Text className='p-4 text-center text-3xl font-bold uppercase tracking-widest text-rose-300'>
           Welcome to the world of Star Wars
-        </Animated.Text>
-      </View>
+        </Text>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
